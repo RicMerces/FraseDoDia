574af0a61881b40e5db5238c83abaaf6cf7c9c6f
RicMerces <ricardoliveira0703@gmail.com>
2022-04-11 17:59:31 -0300

    Text

--- frasedodia/lib/main.dart
+++ frasedodia/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(FraseDoDia());
@@ -10,6 +11,26 @@ class FraseDoDia extends StatefulWidget {
 }
 
 class _MainState extends State<FraseDoDia> {
+  List<String> _frases = [
+    'Tudo o que um sonho precisa para ser realizado é alguém que acredite que ele possa ser realizado.',
+    'Imagine uma nova história para sua vida e acredite nela.',
+    'Não espere por uma crise para descobrir o que é importante em sua vida.',
+    'Acredite em si próprio e chegará um dia em que os outros não terão outra escolha senão acreditar com você.',
+    'Pessimismo leva à fraqueza, otimismo ao poder.',
+    'O otimismo é a fé em ação. Nada se pode levar a efeito sem otimismo.',
+    'O pessimismo, depois de você se acostumar a ele, é tão agradável quanto o otimismo.',
+    'O contrário do pessimismo raramente é o otimismo. O contrário do pessimismo, se não é a boa intenção de injetar força nos fracos, o que é bonito e faz bem, é quase sempre a idiota.',
+  ];
+
+  var _frase = 'Clique abaixo para gerar uma frase';
+
+  void alterarFrase() {
+    int sorteio = Random().nextInt(_frases.length);
+    setState(() {
+      _frase = _frases[sorteio];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,21 +40,24 @@ class _MainState extends State<FraseDoDia> {
           title: const Text('Frase do dia'),
         ),
         body: Center(
+            child: Container(
+          padding: EdgeInsets.all(20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Image.asset('assets/logo.png'),
-              const Text(
-                'Clique abaixo para gerar uma frase',
-                style: TextStyle(
+              Text(
+                _frase,
+                style: const TextStyle(
                   fontSize: 20,
                   fontStyle: FontStyle.italic,
                 ),
+                textAlign: TextAlign.center,
               ),
               TextButton(
-                onPressed: () {},
-                child: Text(
+                onPressed: alterarFrase,
+                child: const Text(
                   'Nova Frase',
                   style: TextStyle(
                     color: Colors.white,
@@ -50,7 +74,7 @@ class _MainState extends State<FraseDoDia> {
               )
             ],
           ),
-        ),
+        )),
       ),
     );
   }
